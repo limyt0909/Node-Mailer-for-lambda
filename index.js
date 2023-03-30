30b00955748a9b17ad42c1369617e41eda87fdaf
I'm Yong Tack <65595274+limyt0909@users.noreply.github.com>
2023-03-30 14:44:25 +0900

    230330

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const nodemailer = require('nodemailer')
 exports.handler = async (event) => {
-let transporter = nodemailer.createTransport({
-      host: ",
-      port: 25,
-      auth: {
-        user: "",
-        pass: "",
-      },
-    });
+  let transporter = nodemailer.createTransport({
+    host: "",
+    port: 25,
+    auth: {
+      user: "",
+      pass: "",
+    },
+  });
 
-    let info = await transporter.sendMail({
-    
-      from: "no-reply",
-      to: event.to,
-      subject: event.subject,
-      text:event.text
- 
-      `,
-    });
-    return { success: true, reason: "메일 보내기 성공" };
-  } catch (e) {
-    logger.error(e);
-    return { success: false, reason: "메일 보내기 실패" };
-  }
+  let info = await transporter.sendMail({
+
+    from: "no-reply",
+    to: event.to,
+    subject: event.subject,
+    text: event.text
+
+  })
+  return { success: true, reason: "메일 보내기 성공" };
+} catch (e) {
+  logger.error(e);
+  return { success: false, reason: "메일 보내기 실패" };
 }
+
